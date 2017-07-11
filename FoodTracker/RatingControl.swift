a91498aee58041770b6087564c792e65af207002
David Janas <davidjanasr@gmail.com>
2017-07-11 15:48:43 -0400

    Initialize rating control view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by David Janas on 7/11/17.
+//  Copyright © 2017 David Janas. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
